8f66105f7498e8e0c48fcdf82471203e1499094f
nurcanNT <nrcntpkra@gmail.com>
2024-07-06 21:53:52 +0300

    feat: develop localstorage redux

--- src/store.js
+++ src/store.js
@@ -2,9 +2,36 @@ import { createStore, combineReducers } from 'redux';
 import authReducer from './reducers/authReducer';
 
 const rootReducer = combineReducers({
-  auth: authReducer
-});
+    auth: authReducer
+  });
 
-const store = createStore(rootReducer);
-
-export default store;
+  const saveToLocalStorage = (state) => {
+    try {
+      const serializedState = JSON.stringify(state);
+      localStorage.setItem('state', serializedState);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  
+  const loadFromLocalStorage = () => {
+    try {
+      const serializedState = localStorage.getItem('state');
+      if (serializedState === null) return undefined;
+      return JSON.parse(serializedState);
+    } catch (e) {
+      console.error(e);
+      return undefined;
+    }
+  };
+  
+  const persistedState = loadFromLocalStorage();
+  
+  const store = createStore(
+    rootReducer,
+    persistedState
+  );
+  
+  store.subscribe(() => saveToLocalStorage(store.getState()));
+  
+  export default store;
